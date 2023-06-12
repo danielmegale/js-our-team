89d7167803f60703bebf7244bd95db6d4c3eeaac
danielmegale <danim1@live.it>
2023-06-12 16:51:21 +0200

    added css js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('js')
+const list=document.getElementById('list')
+
+const teamList=[
+    {
+        Img:'img/wayne-barnett-founder-ceo.jpg',
+        name:'Wayne Barnett',
+        position:'Founder & CEO'
+    },
+    {
+        Img:'img/angela-caroll-chief-editor.jpg',
+        name:'Angela Caroll',
+        position:'Chief Editor'
+    },
+    {
+        Img:'img/walter-gordon-office-manager.jpg',
+        name:'Walter Gordon',
+        position:'Office Manager'
+    },
+    {
+        Img:'img/angela-lopez-social-media-manager.jpg',
+        name:'Angela Lopez',
+        position:'Social Media Manager'
+    },
+    {
+        Img:'img/scott-estrada-developer.jpg',
+        name:'Scott Estrada',
+        position:'Developer'
+    },
+    {
+        Img:'img/barbara-ramos-graphic-designer.jpg',
+        name:'Barbara Ramos',
+        position:'Graphic Designer'
+    }
+]
+let team='';
+for(let i=0;i<teamList.length;i++){
+    const teamMates= teamList[i];
+    team +=`
+    <li>
+        <img src="${teamMates.Img}" alt="">
+        <h4>${teamMates.name} </h4>
+        <p>${teamMates.position}</p>
+    </li>
+    `;
+    console.log(team)
+}
+list.innerHTML=team;
---
+++ steps.md
@@ -0,0 +1,7 @@
+# OUR TEAM
+1. Prendo un oggeto dal DOM
+2. Creo un array
+3. Creo gli oggetti dentro l'array
+4. Stampo in console gli oggetti
+5. Creo degli elementi da inserire nel DOM
+6. Stampo gli elementi con gli ogetti nel DOM
